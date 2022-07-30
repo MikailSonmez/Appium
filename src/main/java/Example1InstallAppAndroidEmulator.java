dec5df6d785b4e2ba81da1b04a322303985a4ba0
Mikail Sonmez <mikail4458@hotmail.com>
2022-07-30 13:03:02 +0300

    v1

---
+++ src/main/java/Example1InstallAppAndroidEmulator.java
@@ -0,0 +1,31 @@
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Example1InstallAppAndroidEmulator {
+
+    public static void main(String[] args) throws MalformedURLException {
+
+        AndroidDriver<MobileElement> driver;
+
+        DesiredCapabilities dc = new DesiredCapabilities();
+
+        dc.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
+
+        dc.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+
+        dc.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9"); // version number here
+
+        dc.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
+
+        // dc.setCapability(MobileCapabilityType.APP,"C:\\apkfiles\\asdas.apk");
+
+        driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub/"), dc);
+
+        driver.quit();
+    }
+}
